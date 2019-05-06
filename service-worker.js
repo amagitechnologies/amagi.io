f859ef64448c6447e6c3e84296d919074c621c7a
Thomas Laughlin <tmlaughlinjr@gmail.com>
2019-05-06 18:53:59 +0800

    May 6 update

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8493269942c55a4cbffa8df7b57cc5b8.js"
+  "/amagi.io/precache-manifest.47a6895fda70302537405e4c014fcbec.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/amagi.io/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
